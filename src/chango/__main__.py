4e209d2913b18f63082498d3f8f20e6cfbb4dc69
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-07-24 20:56:14 +0200

    allow `python -m chango`

--- src/chango/__main__.py
+++ src/chango/__main__.py
@@ -1,8 +1,10 @@
 #  SPDX-FileCopyrightText: 2024-present Hinrich Mahler <chango@mahlerhome.de>
 #
 #  SPDX-License-Identifier: MIT
+#
+#  SPDX-License-Identifier: MIT
 
-from .main import app
+from ._cli import app
 
 if __name__ == "__main__":
     app()
--- src/chango/_cli/__init__.py
+++ src/chango/_cli/__init__.py
@@ -1,3 +1,40 @@
-#  SPDX-FileCopyrightText: 2024-present Hinrich Mahler <chango@mahlerhome.de>
+# SPDX-FileCopyrightText: 2024-present Hinrich Mahler <chango@mahlerhome.de>
 #
-#  SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: MIT
+__all__ = ["app"]
+
+from typing import Annotated
+
+import typer
+
+from chango._cli.utils.common import description
+
+from .. import __version__
+from .edit import edit
+from .new import new
+from .release import release
+from .report import app as report_app
+
+app = typer.Typer(help=f"CLI for chango - {description}")
+
+
+def version_callback(value: bool):
+    if value:
+        typer.echo(__version__)
+        raise typer.Exit
+
+
+@app.callback()
+def main(
+    _version: Annotated[
+        bool,
+        typer.Option("--version", callback=version_callback, help="Show the version and exit."),
+    ] = False,
+):
+    pass
+
+
+app.command()(edit)
+app.command()(new)
+app.command()(release)
+app.add_typer(report_app, name="report")
