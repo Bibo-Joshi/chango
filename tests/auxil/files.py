1f8112708aff6a923e230eecb6756a52a7a646a8
Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>
2024-11-03 16:37:14 +0100

    Remove Redundant Logic From and Add Unit Tests for `HeaderVersionHistory` (#19)

--- tests/auxil/files.py
+++ tests/auxil/files.py
@@ -3,9 +3,11 @@
 #  SPDX-License-Identifier: MIT
 from pathlib import Path
 
+from chango._utils.types import PathLike
+
 PROJECT_ROOT_PATH = Path(__file__).parent.parent.parent.resolve()
 TEST_DATA_PATH = PROJECT_ROOT_PATH / "tests" / "data"
 
 
-def data_file(filename: str) -> Path:
+def data_file(filename: PathLike) -> Path:
     return TEST_DATA_PATH / filename
