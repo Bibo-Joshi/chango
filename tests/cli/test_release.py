1756875f47179f5299d1ccf92a5dc4b6640ae47c
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-12-09 22:03:54 +0100

    rename a test case

--- tests/cli/test_release.py
+++ tests/cli/test_release.py
@@ -13,9 +13,7 @@ from tests.cli.conftest import ReuseCliRunner
 
 class TestRelease:
     @pytest.mark.parametrize("has_unreleased", [True, False])
-    def test_release_no_unreleased(
-        self, cli: ReuseCliRunner, mock_chango_instance, has_unreleased
-    ):
+    def test_release_basic(self, cli: ReuseCliRunner, mock_chango_instance, has_unreleased):
         mock_chango_instance.release.return_value = has_unreleased
         result = cli.invoke(args=["release", "--uid", "1.0.0", "--date", "2024-01-01"])
         assert len(mock_chango_instance.release.call_args_list) == 1
