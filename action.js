7b4b558f101ef1089f48c13edef9a0e2423f3156
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2025-02-02 17:10:11 +0100

    try going step by step

--- action.js
+++ action.js
@@ -12,76 +12,76 @@ async function main (github, context, core, query_issue_types) {
         return;
     }
 
-    // Fetch the linked issues
-    const data = await github.graphql(`
-        query {
-            repository(owner:"${context.repo.owner}", name:"${context.repo.repo}") {
-                pullRequest(number:${context.payload.pull_request.number}) {
-                    closingIssuesReferences(first: 100) {
-                        nodes {
-                            number
-                            title
-                            ${query_issue_types ? 'issueType { name }' : ''}
-                            labels(first: 100) {
-                                nodes {
-                                    name
-                                    id
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    `);
-
-    const closingIssues = data.repository.pullRequest.closingIssuesReferences.nodes.map(node => {
-        return {
-            number: node.number,
-            title: node.title,
-            labels: node.labels.nodes.map(label => label.name),
-            issueType: node.issueType ? node.issueType.name : null
-        };
-    });
-
-    // Fetch the parent PR
-    const response = await github.graphql(`
-        query {
-            search(
-                type: ISSUE,
-                query: "org:${context.repo.owner} repo:${context.repo.repo} is:pr base:main head:${context.payload.pull_request.base.ref}"
-                first: 100
-            ) {
-                issueCount
-                nodes {
-                    ... on PullRequest {
-                        number
-                        title
-                        url
-                        state
-                        author {
-                            login
-                        }
-                    }
-                }
-            }
-        }
-    `);
-    const prs = response.search.nodes;
-    const parentPR = prs.length > 0 ? prs[0] : null;
-    if (parentPR) {
-        parentPR.author_login = parentPR.author.login;
-        delete parentPR.author;
-    }
-
-    // Combine the linked issues and the parent PR to a single JSON object
-    const linkedIssuesAndParentPR = {
-        linked_issues: closingIssues,
-        parent_pull_request: parentPR
-    };
-    // debug-print the context
-    console.log(JSON.stringify(linkedIssuesAndParentPR, null, 2));
-    core.setOutput('data', JSON.stringify(linkedIssuesAndParentPR));
+    // // Fetch the linked issues
+    // const data = await github.graphql(`
+    //     query {
+    //         repository(owner:"${context.repo.owner}", name:"${context.repo.repo}") {
+    //             pullRequest(number:${context.payload.pull_request.number}) {
+    //                 closingIssuesReferences(first: 100) {
+    //                     nodes {
+    //                         number
+    //                         title
+    //                         ${query_issue_types ? 'issueType { name }' : ''}
+    //                         labels(first: 100) {
+    //                             nodes {
+    //                                 name
+    //                                 id
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `);
+    //
+    // const closingIssues = data.repository.pullRequest.closingIssuesReferences.nodes.map(node => {
+    //     return {
+    //         number: node.number,
+    //         title: node.title,
+    //         labels: node.labels.nodes.map(label => label.name),
+    //         issueType: node.issueType ? node.issueType.name : null
+    //     };
+    // });
+    //
+    // // Fetch the parent PR
+    // const response = await github.graphql(`
+    //     query {
+    //         search(
+    //             type: ISSUE,
+    //             query: "org:${context.repo.owner} repo:${context.repo.repo} is:pr base:main head:${context.payload.pull_request.base.ref}"
+    //             first: 100
+    //         ) {
+    //             issueCount
+    //             nodes {
+    //                 ... on PullRequest {
+    //                     number
+    //                     title
+    //                     url
+    //                     state
+    //                     author {
+    //                         login
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `);
+    // const prs = response.search.nodes;
+    // const parentPR = prs.length > 0 ? prs[0] : null;
+    // if (parentPR) {
+    //     parentPR.author_login = parentPR.author.login;
+    //     delete parentPR.author;
+    // }
+    //
+    // // Combine the linked issues and the parent PR to a single JSON object
+    // const linkedIssuesAndParentPR = {
+    //     linked_issues: closingIssues,
+    //     parent_pull_request: parentPR
+    // };
+    // // debug-print the context
+    // console.log(JSON.stringify(linkedIssuesAndParentPR, null, 2));
+    // core.setOutput('data', JSON.stringify(linkedIssuesAndParentPR));
 }
 
 module.exports = main;
